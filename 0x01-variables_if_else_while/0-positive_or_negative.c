799718342b8f99a3521f836b8b1389296f0bd834
Chi-Naydu <johnvianneychinedu@gmail.com>
2023-02-17 14:45:56 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* imported the librabries I need to run the program */
 
 /**
-* main - program function starts here
-*
-* Return: 0 (when no error)
-*/
+ * main - prints if the number is positive,negative or 0
+ * Return: Always 0
+ */
 int main(void)
 {
+	int;
 
-int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* conditional statements starts here */
+	if ( > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 
-int lastDigit = n % 10;
-
-printf("Last digit of %d is %d\n", n, lastDigit);
-
-if (lastDigit > 5)
-{
-printf("and is greater than 5\n");
-}
-else if (lastDigit == 0)
-{
-printf("and is 0");
-}
-else if (lastDigit < 6)
-{
-printf("and is less than 6 and not 0\n");
-return (0);
+	return (0);
 }
