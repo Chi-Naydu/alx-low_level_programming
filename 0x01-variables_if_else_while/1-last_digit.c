d7d3129ef966f455981f31c21f23a087a1e0b7fe
Chi-Naydu <johnvianneychinedu@gmail.com>
2023-02-19 16:39:50 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,25 +4,33 @@
 /* imported the librabries I need to run the program */
 
 /**
-*i main - program function starts here
+* main - program function starts here
 *
 * Return: 0 (when no error)
 */
 int main(void)
 {
+
 int n;
-int lastdigit;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* conditional statements starts here */
 
-int lastDigit = n % 10
-	printf("%d" lastdigit\n);
-int lastdigit > 5
-	printf("%d" and is greater than 5\n);
-else if lastdigit = 0
-	printf("%d" and is 0\n);
-else lastdigit < 6 
-	printf("%d" and is less than 6 and not 0\n);
-Return(0);
+int lastDigit = n % 10;
+
+printf("Last digit of %d is %d\n", n, lastDigit);
+
+if (lastDigit > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (lastDigit == 0)
+{
+printf("and is 0");
+}
+else if (lastDigit < 6)
+{
+printf("and is less than 6 and not 0\n");
+return (0);
 }
